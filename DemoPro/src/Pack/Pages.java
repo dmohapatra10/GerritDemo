d9716244bcba93389d0aefbe8e7150266e747c4c
dmohapatra <dmohapatra@myvest.com>
2020-07-02 12:04:52 +0530

    CICU-6 added a new Print Statement for Demo purpose

--- DemoPro/src/Pack/Pages.java
+++ DemoPro/src/Pack/Pages.java
@@ -5,7 +5,8 @@ public class Pages {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Added a new POM class");
-	
+		
+		System.out.println("Demo");
 
 	}
 
