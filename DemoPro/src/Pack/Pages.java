da04a18b0374b159bf319bf58491daba826cd3f6
dmohapatra <dmohapatra@myvest.com>
2020-07-02 21:56:55 +0530

    CICU-7 connecting github to jira without Git Integration App
    Change-Id: Id33796bb9e589437d80703fac2ee9baca91fca42

--- DemoPro/src/Pack/Pages.java
+++ DemoPro/src/Pack/Pages.java
@@ -5,7 +5,7 @@ public class Pages {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Added a new POM class");
-		
+		System.out.println("Connecting to Jira without Git Integration App");
 		
 
 	}
