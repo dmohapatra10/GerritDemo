073768901f41ce886b5e621f987995dad302333f
dmohapatra <dmohapatra@myvest.com>
2020-07-02 22:11:16 +0530

    CICU-7 adding a comment at 22:11
    Change-Id: I7a75772fa2a581e6e367649f8658b65ea2901c1c

--- DemoPro/src/Pack/Pages.java
+++ DemoPro/src/Pack/Pages.java
@@ -6,7 +6,7 @@ public class Pages {
 		// TODO Auto-generated method stub
 		System.out.println("Added a new POM class");
 		System.out.println("Connecting to Jira without Git Integration App");
-		
+		System.out.println("Adding another print statement");
 
 	}
 
